a3bd75b143b83aaa7e799bbb1d66e3bf6c329d94
SiriusStarr <2049163+SiriusStarr@users.noreply.github.com>
2022-11-02 12:55:06 -0700

    Add flake for development

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "My keyboard firmware";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+
+    kaleidoscope = {
+      url = "github:keyboardio/Kaleidoscope";
+      flake = false;
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    kaleidoscope,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            arduino-cli
+          ];
+
+          shellHook = ''
+            export KALEIDOSCOPE_DIR=${kaleidoscope}
+            export ARDUINO_DIRECTORIES_USER=/tmp/arduino/user
+            export ARDUINO_DIRECTORIES_DATA=/tmp/arduino/data
+            export ARDUINO_DIRECTORIES_DOWNLOADS=/tmp/arduino/downloads
+            make -f ${kaleidoscope}/Makefile setup
+          '';
+        };
+      }
+    );
+}
