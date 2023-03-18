c3481299b2611f64ad516a2bbc6c88eaf83dc046
SiriusStarr <2049163+SiriusStarr@users.noreply.github.com>
2023-03-18 16:17:26 -0700

    ✨ Add keymap-drawer to flake

--- flake.nix
+++ flake.nix
@@ -9,6 +9,16 @@
       flake = false;
     };
 
+    keymap-drawer-src = {
+      url = "github:caksoylar/keymap-drawer";
+      flake = false;
+    };
+
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -16,12 +26,38 @@
     self,
     nixpkgs,
     kaleidoscope,
+    keymap-drawer-src,
+    poetry2nix,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem
     (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlay = nixpkgs.lib.composeManyExtensions [
+          poetry2nix.overlay
+          (final: prev: {
+            keymap-drawer = prev.poetry2nix.mkPoetryApplication {
+              projectDir = keymap-drawer-src;
+
+              overrides =
+                prev.poetry2nix.defaultPoetryOverrides.extend
+                (self: super: {
+                  deptry =
+                    super.deptry.overridePythonAttrs
+                    (
+                      old: {
+                        buildInputs = (old.buildInputs or []) ++ [super.poetry];
+                      }
+                    );
+                });
+            };
+          })
+        ];
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [overlay];
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -35,7 +71,7 @@
             export ARDUINO_DIRECTORIES_DATA=/tmp/arduino/data
             export ARDUINO_DIRECTORIES_DOWNLOADS=/tmp/arduino/downloads
             make -f ${kaleidoscope}/Makefile setup
-            export PATH="${kaleidoscope}/bin:$PATH"
+            export PATH="${kaleidoscope}/bin:${pkgs.keymap-drawer}/bin:$PATH"
           '';
         };
       }
