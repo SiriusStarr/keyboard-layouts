3c8dfd333e9a82be9848ab4326ea00cf55844cf6
SiriusStarr <2049163+SiriusStarr@users.noreply.github.com>
2024-02-14 11:39:14 -0800

    Disable adaptive keys

--- QMK/users/SiriusStarr/config.h
+++ QMK/users/SiriusStarr/config.h
@@ -3,7 +3,8 @@
 #define PERMISSIVE_HOLD   // Resolve to held value for a key pressed and released while mod-tap is held
 
 // Adaptive keys
-#define ADAPTIVE_KEYS_ENABLE
-#define ADAPTIVE_TERM 125  // Rolling threshold
+// No longer used
+// #define ADAPTIVE_KEYS_ENABLE
+// #define ADAPTIVE_TERM 125  // Rolling threshold
 
 // #define CONSOLE_KEY_LOGGER_ENABLE  // Turn on keylogging for heat maps.
