5fd5103fbaa07a375da5dc88d50b75c4325783a5
SiriusStarr <2049163+SiriusStarr@users.noreply.github.com>
2025-02-06 11:04:35 -0800

    Remove keymap-drawer and do it in CI

--- flake.nix
+++ flake.nix
@@ -9,16 +9,6 @@
       flake = false;
     };
 
-    keymap-drawer-src = {
-      url = "github:caksoylar/keymap-drawer";
-      flake = false;
-    };
-
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -26,37 +16,13 @@
     self,
     nixpkgs,
     kaleidoscope,
-    keymap-drawer-src,
-    poetry2nix,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem
     (
       system: let
-        overlay = nixpkgs.lib.composeManyExtensions [
-          poetry2nix.overlays.default
-          (final: prev: {
-            keymap-drawer = prev.poetry2nix.mkPoetryApplication {
-              projectDir = keymap-drawer-src;
-
-              overrides =
-                prev.poetry2nix.defaultPoetryOverrides.extend
-                (self: super: {
-                  deptry =
-                    super.deptry.overridePythonAttrs
-                    (
-                      old: {
-                        buildInputs = (old.buildInputs or []) ++ [super.poetry];
-                      }
-                    );
-                });
-            };
-          })
-        ];
-
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [overlay];
         };
       in {
         devShells.default = pkgs.mkShell {
@@ -76,7 +42,7 @@
               export ARDUINO_DIRECTORIES_DATA=/tmp/arduino/data
               export ARDUINO_DIRECTORIES_DOWNLOADS=/tmp/arduino/downloads
               make -f ${kaleidoscope}/Makefile setup
-              export PATH="${kaleidoscope}/bin:${pkgs.keymap-drawer}/bin:$PATH"
+              export PATH="${kaleidoscope}/bin:$PATH"
             '';
 
             qmkSetup = ''
